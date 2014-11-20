ea3a54d4b22a56b5a8c8a18ee8bfdc0c7cbf711e
pmartinperez <pmartinperez@eniac24.danielcastelao.org>
2014-11-20 08:49:15 +0100

    declaracion de variables y print2 suma

--- src/pruebacommits/PruebaCommits.java
+++ src/pruebacommits/PruebaCommits.java
@@ -6,8 +6,9 @@ public class PruebaCommits {
 
    
     public static void main(String[] args) {
+        int a=2, b=3;
         System.out.println("Hula commit");
-       
+        System.out.println(a+b);
     }
     
 }
