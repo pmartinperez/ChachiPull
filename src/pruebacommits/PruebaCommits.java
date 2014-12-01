2b16c0479a406d9afa120090822ba4f1cdf9995e
pmartinperez <pmartinperez@eniac24.danielcastelao.org>
2014-12-01 10:07:05 +0100

    strings

--- src/pruebacommits/PruebaCommits.java
+++ src/pruebacommits/PruebaCommits.java
@@ -1,5 +1,6 @@
 package pruebacommits;
 
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class PruebaCommits {
@@ -9,6 +10,21 @@ public class PruebaCommits {
         JOptionPane.showMessageDialog(null, "Hula commit");
         JOptionPane.showMessageDialog(null, (a + b));
         JOptionPane.showMessageDialog(null, "Fin programa");
+        
+        Scanner dato = new Scanner(System.in);
+        for(int i=0;i<5;i++){
+            String nombre;
+            System.out.println("Introduce nombre");
+            nombre=dato.next();
+            nombre= nombre.toLowerCase();
+            nombre=nombre.toUpperCase();
+            
+           // if(nombre=="Brais")
+            if(nombre.equals("bRais"))
+                System.out.println("está Brais");  
+            else
+                System.out.println("No esta Brais");
+        }
     }
 
 }
